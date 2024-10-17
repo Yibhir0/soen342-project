bb2d71b422807e9ed50c252aaa4bfbad75bffdfc
AbdelmalekMouhamou <146683074+AbdelmalekMouhamou@users.noreply.github.com>
2024-10-17 13:59:16 -0400

    wrote the logic to create new offering items

--- Iteration2/implementation/iteration-v1/src/Main.java
+++ Iteration2/implementation/iteration-v1/src/Main.java
@@ -1,4 +1,5 @@
 import dataGenerator.space.SpaceData;
+import offering.Offering;
 import offering.OfferingItem;
 import organisation.Location;
 import organisation.Organisation;
@@ -27,6 +28,33 @@ public class Main {
 
 
 
+public void createOffering(Organisation organisation){
+    //choose location
+    System.out.println("choose a location:");
+    for (Location location : organisation.getLocations()) {
+        System.out.println("1. "+location);
+    }
+
+    //choose space from chosen location
+    //create schedule
+    //enter lesson type
+    //create Offering(String lessonType,Space space, Schedule schedule)
+    //createOfferingItems(Offering offering)
+}
+public void createOfferingItems(Offering offering){
+    String input="y";
+    while(!input.equals("n")||!input.equals("N")||!input.equals("no")||!input.equals("NO")) {
+
+        System.out.print("Add offering item? (y/n):");
+        System.out.print("Enter start time:");//check if start time is between offering.Schedule.startTime and  offering.Schedule.endTime
+        System.out.print("Enter end time:");//same for end time
+        System.out.print("Is this offering public? (y/n):");
+        // create OfferingItem(boolean isPrivate, LocalTime startTime, LocalTime endTime)
+        //offering.add(offeringItem)
+
+    }
+
+}
 
 
 }
