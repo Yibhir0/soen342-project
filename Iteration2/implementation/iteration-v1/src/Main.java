86d0904fad0aef763189f2ce18fa61818cc61f6e
AbdelmalekMouhamou <146683074+AbdelmalekMouhamou@users.noreply.github.com>
2024-10-17 13:36:42 -0400

    added generated data

--- Iteration2/implementation/iteration-v1/src/Main.java
+++ Iteration2/implementation/iteration-v1/src/Main.java
@@ -1,14 +1,26 @@
 import dataGenerator.space.SpaceData;
+import offering.OfferingItem;
+import organisation.Location;
+import organisation.Organisation;
 import organisation.Space;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
 
     public static void main(String[] args) {
+        Organisation organisation=new Organisation("");
+        //generating location and space data (temporary until database is implemented)
+        organisation.storeLocations(SpaceData.generateLocations());
+        ArrayList<Location> locations=organisation.getLocations();
+        for(var loc:locations){
+            ArrayList<Space> generatedSpaces=SpaceData.generateSpaces(loc);
+            loc.storeSpaces(generatedSpaces);
+        }
 
-        List<Space> spaces = SpaceData.generateSpaces();
 
     }
 
--- Iteration2/implementation/iteration-v1/src/dataGenerator/space/SpaceData.java
+++ Iteration2/implementation/iteration-v1/src/dataGenerator/space/SpaceData.java
@@ -1,5 +1,6 @@
 package dataGenerator.space;
 
+import organisation.City;
 import organisation.Location;
 import organisation.Space;
 
@@ -10,10 +11,12 @@ public class SpaceData {
 
 
 
-    public static ArrayList<Space> generateSpaces() {
+    public static ArrayList<Space> generateSpaces(Location location) {
 
         // to do
         ArrayList<Space> spaces = new ArrayList<>();
+        spaces.add(new Space("Gym",location));
+        spaces.add(new Space("Pool",location));
 
         return spaces;
 
@@ -23,7 +26,9 @@ public class SpaceData {
 
         // to do
         ArrayList<Location> locations = new ArrayList<>();
-
+        City mtl=new City("Montreal","Quebec","Canada");
+        locations.add(new Location("EV. Building", "123 Mackay St",mtl));
+        locations.add(new Location("Faubourg", "123 St-Catherine",mtl));
         return locations;
 
     }
