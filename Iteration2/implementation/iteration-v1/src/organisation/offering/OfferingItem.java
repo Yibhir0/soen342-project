864f60261c4767c1ca0aebdfeb8852aa722b79ad
AbdelmalekMouhamou <146683074+AbdelmalekMouhamou@users.noreply.github.com>
2024-10-30 17:54:44 -0400

    Update OfferingItem.java
    -added toString for admin and for public
    -added booking methods

--- Iteration2/implementation/iteration-v1/src/organisation/offering/OfferingItem.java
+++ Iteration2/implementation/iteration-v1/src/organisation/offering/OfferingItem.java
@@ -1,5 +1,6 @@
 package organisation.offering;
 
+import organisation.user.Client;
 import organisation.user.Instructor;
 
 import java.time.LocalTime;
@@ -9,11 +10,12 @@ public class OfferingItem {
     private boolean isPrivate=false ;
     private LocalTime startTime;
 
-    private boolean isAvailableToPublic = false;
+    private boolean isAvailable = false;
 
     private LocalTime endTime;
     private Instructor instructor;
     private Offering offering;
+    private Client client;
 
     public OfferingItem(boolean isPrivate, LocalTime startTime, LocalTime endTime) {
         this.isPrivate = isPrivate;
@@ -36,18 +38,51 @@ public class OfferingItem {
     public void setOffering(Offering offer){
         this.offering=offer;
     }
-    public void setAvailableToPublic() {
-        isAvailableToPublic = !this.isAvailableToPublic ;
-    }
+public boolean book(Client client){
+        if(!isAvailable){
+            return false;
+        }
+        else{
+            this.client=client;
+            isAvailable=false;
+            return true;
+        }
+}
+public void removeBooking(){
+        this.client=null;
+        this.isAvailable=true;
+}
+public boolean isAvailable(){
+        return isAvailable;
+}
 
     public String toString(){
         String type= isPrivate?"Private":"Group";
         String instructorName=instructor!=null?instructor.getName():"N/A";
         return startTime + " - " + endTime +". " + type + ". Instructor: " + instructorName ;
     }
+
+    public String toStringForPublic(){
+        if(isAvailable){
+        return this.toString();
+        }
+        else{
+            return startTime + " - " + endTime +". Not available.";
+        }
+    }
+
+    //doesn't display instructor name
     public String toStringForInstructors(){
         String type= isPrivate?"Private":"Group";
         return offering.getSpace() + " - " + endTime +". " + type;
+    }
+    //shows client name
+    public String toStringForAdmin(){
+        String booking="";
+        if(client!=null){
+            booking=". booked by:"+client.getUsername();
+        }
+        return this.toString()+booking;
     }
 
 }
