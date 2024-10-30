500df1cdcf78e8993fb2b9c6aa6b94b92416a825
AbdelmalekMouhamou <146683074+AbdelmalekMouhamou@users.noreply.github.com>
2024-10-30 17:53:46 -0400

    changed name to username

--- Iteration2/implementation/iteration-v1/src/organisation/user/Administrator.java
+++ Iteration2/implementation/iteration-v1/src/organisation/user/Administrator.java
@@ -1,13 +1,12 @@
 package organisation.user;
 
 public class Administrator implements User {
-    private final String name = "admin";
-    private final String phone = "1234567890";
+    private final String username = "admin";
     private final String password = "admin";
 
 
     public int login(String name, String password) {
-        if (this.name.equals(name) && this.password.equals(password)) {
+        if (this.username.equals(name) && this.password.equals(password)) {
             return 1;
         } else {
             return 0;
