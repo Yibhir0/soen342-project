7420b7aa8d912308f08161f8239385cf2bb3f8c1
AbdelmalekMouhamou <146683074+AbdelmalekMouhamou@users.noreply.github.com>
2024-10-30 17:55:54 -0400

    Update Organisation.java
    -added methods to view offerings for each type of user
    -added clients attributes and getter/setter

--- Iteration2/implementation/iteration-v1/src/organisation/Organisation.java
+++ Iteration2/implementation/iteration-v1/src/organisation/Organisation.java
@@ -2,6 +2,8 @@ package organisation;
 
 import organisation.offering.Offering;
 import organisation.Locations.Location;
+import organisation.offering.OfferingItem;
+import organisation.user.Client;
 import organisation.user.Instructor;
 
 import java.util.ArrayList;
@@ -12,6 +14,7 @@ public class Organisation {
 
     private ArrayList<Instructor> instructors;
     private ArrayList<Offering> offerings;
+    private ArrayList<Client> clients;
 
     public Organisation(String name) {
         this.name = name;
@@ -50,5 +53,67 @@ public class Organisation {
         return this.instructors;
     }
 
+    public ArrayList<Client> getClients() {
+        return clients;
+    }
+    public void setClients(ArrayList<Client> Clients) {
+        this.clients = clients;
+    }
+
+    /**
+     * view all  available offerings that are taken by an Instructor (public can't see offerings not taken)
+     */
+    public  void viewOfferingsForPublic() {
+        for (var offering :offerings) {
+            offering.printOfferingsForPublic();
+        }
+    }
+
+    /**
+     * view all offerings
+     */
+    public  void viewAllOfferingsForAdmin() {
+        for (var offering :offerings) {
+            offering.printOfferingsForAdmin();
+        }
+    }
+
+
+    /**
+     * Display available offerings to take and offerings already taken by the instructor
+     * @param instructor
+     */
+   public void viewAvailableOfferingsForInstructors(Instructor instructor) {
+
+        ArrayList<OfferingItem> availableInstructorItems = getAvailableOfferings(instructor);
+        if(availableInstructorItems.isEmpty()){
+            System.out.println("No available Offerings.");
+            return;
+        }
+        for (var offeringItem : availableInstructorItems) {
+            System.out.println(offeringItem);
+        }
+
+    }
+    /**
+     *
+     * @param instructor
+     * @return
+     */
+    public ArrayList<OfferingItem> getAvailableOfferings(Instructor instructor) {
+        ArrayList<OfferingItem> availableOfferings = new ArrayList<>();
+        for (var offering : getOfferings()) {
+            if (offering.getLessonType().equals(instructor.getSpeciality())) {
+                for(var item:offering.getOfferingItemList()){
+                    if(!item.hasInstructor()){
+                        availableOfferings.add(item);
+                    }
+
+                }
+
+            }
+        }
+        return availableOfferings;
+    }
 
 }
